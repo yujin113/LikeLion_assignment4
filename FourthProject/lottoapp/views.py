04e2f98b025d72fc82ad2717582ef5a0bbbebf27
yujin <claire9585@sookmyung.ac.kr>
2021-05-05 21:37:47 +0900

    랜덤 짝궁 구하기

--- FourthProject/lottoapp/views.py
+++ FourthProject/lottoapp/views.py
@@ -5,11 +5,10 @@ def home(request):
     return render(request, 'lottoapp/home.html')
 
 def result(request):
-    num = random.sample(range(1,47),6)
+    list = ['김유진', '박혜준', '노은성', '김소은', '강연우', '김정운', '김서영', '이연수', '오예림', '장한빛', '황서경', '안현주', '문다연', '박경나', '이민정', '조원아']
+    partner = random.sample(list, 1)
+    partner = "".join(partner)
 
-    list = ('사과', '딸기', '포도', '배', '키위', '복숭아')
-    fruits = random.sample(list, 2)
-
-    return render(request, 'lottoapp/result.html', {'num': num, 'fruits': fruits})
+    return render(request, 'lottoapp/result.html', {'partner': partner})
 
 # Create your views here.
